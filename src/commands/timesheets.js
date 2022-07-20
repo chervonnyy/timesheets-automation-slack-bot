4053f2a0d59b51d259a9a6feb34f67668a702646
Vlad Chervonnyy <vchervonnyy@gmail.com>
2022-07-21 00:46:08 +0400

    timesheets repond update

--- src/commands/timesheets.js
+++ src/commands/timesheets.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const formatMessage = require('../formatMessage/timesheets');
 
-const timesheetsCommand = app => async ({ command, ack, respond, say }) => {
+const timesheetsCommand = app => async ({ command, ack, respond }) => {
   await ack();
 
   const dates = command.text.trim().split('-');
@@ -10,30 +10,37 @@ const timesheetsCommand = app => async ({ command, ack, respond, say }) => {
   if (!dates || dates.length !== 2) {
     return respond({
       response_type: 'ephemeral',
-      text: `You have to specify dates. For example: /macys_timesheets 01/07/22-25/07/22`
+      text: `You have to specify dates. For example: /macys_timesheets 01/03/2022-25/03/2022`
     });
   }
 
   try {
-    const { success, errorMsg, result } = await axios.post(process.env.ENDPOINT, {
+    const { data } = await axios.post(process.env.ENDPOINT, {
       dateFrom: dates[0],
       dateTo: dates[1],
       username
-    }, {
-      cmd: 'whoNotSubmit'
     });
+
+    const { success, errorMsg, result } = data;
 
     if (!success) throw new Error(errorMsg || 'Unhandled error');
 
     const users = await Promise.all(result.map(async (rawUser) => {
-      const { user } = await app.client.users.lookupByEmail({ email: rawUser.email });
-      return await { ...rawUser, slackUsername: user.name };
+      let slackUsername;
+      try {
+        const { user } = await app.client.users.lookupByEmail({ email: rawUser.email });
+        slackUsername = user.name;
+      } catch(error) {
+        slackUsername = rawUser.email.split('@')[0];
+      }
+      return await { ...rawUser, slackUsername };
     }));
   
-    say({
+    respond({
       mrkdwn: true,
       blocks: formatMessage(users),
       text: 'Timesheets reminder',
+      response_type: 'in_channel',
     });
 
   } catch (error) {
